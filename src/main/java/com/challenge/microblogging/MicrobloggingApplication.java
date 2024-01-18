678d634592ca6c091008ce3ce7d5c6911623617a
María Sol Llull <llullmariasol@gmail.com>
2024-01-17 23:11:38 -0300

    delete unused file

--- src/main/java/com/challenge/microblogging/MicrobloggingApplication.java
+++ src/main/java/com/challenge/microblogging/MicrobloggingApplication.java
@@ -2,10 +2,11 @@ package com.challenge.microblogging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories(basePackages = "com.challenge.microblogging.repository")
 public class MicrobloggingApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MicrobloggingApplication.class, args);
 	}
